7734cdd85bc65d29ef0987330c4c1db6e36cf0c9
Erick Livia <mister_01@outlook.es>
2025-11-01 07:07:42 -0500

    build admin

--- build.sh
+++ build.sh
@@ -10,3 +10,5 @@ python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python manage.py migrate
+
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); import os; username=os.environ.get('DJANGO_SUPERUSER_USERNAME'); email=os.environ.get('DJANGO_SUPERUSER_EMAIL'); password=os.environ.get('DJANGO_SUPERUSER_PASSWORD'); User.objects.filter(username=username).exists() or User.objects.create_superuser(username, email, password)" | python manage.py shell
